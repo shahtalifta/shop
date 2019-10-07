609cdcf083f68e61d228e1bbbbfb8cb14ff03e4f
shahtalifta <shahtalifta@gmail.com>
2019-10-07 18:35:24 +0300

    add sleep

--- test_items.py
+++ test_items.py
@@ -5,11 +5,10 @@ import time
 
 
 class TestShop:
-    def test_items_one(self, browser):
+    def test_running_auto_tests_for_different_interface_languages(self, browser):
         browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
         button_basket = WebDriverWait(browser, 3).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, "#add_to_basket_form > button[type=\'submit\']")))
-        time.sleep(5)
         assert button_basket
 
-
+        time.sleep(30)
